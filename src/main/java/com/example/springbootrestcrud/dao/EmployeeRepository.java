ab395b0e0c0633179177bab08b0f9cd1c06c9f9e
Nataliya <nataliyasjmonova@gmail.com>
2025-02-01 12:09:04 +0100

    simple Spring security

--- src/main/java/com/example/springbootrestcrud/dao/EmployeeRepository.java
+++ src/main/java/com/example/springbootrestcrud/dao/EmployeeRepository.java
@@ -2,10 +2,7 @@ package com.example.springbootrestcrud.dao;
 
 import com.example.springbootrestcrud.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-
-@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
---
+++ src/main/java/com/example/springbootrestcrud/service/EmployeeService.java
@@ -0,0 +1,17 @@
+package com.example.springbootrestcrud.service;
+
+import com.example.springbootrestcrud.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    List<Employee> findAll();
+
+    Employee findById(int theId);
+
+    Employee save(Employee theEmployee);
+
+    void deleteById(int theId);
+
+}
